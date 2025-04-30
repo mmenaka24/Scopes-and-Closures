c6b2403110997ea964bcffc3540ada39f240f754
Menaka Maruthainar <menaka.maruthainar@softwire.com>
2025-04-30 15:04:55 +0100

    adds user input
    user can now input scores

--- find-high-score.js
+++ find-high-score.js
@@ -1,4 +1,6 @@
-function findHighScore(scores) {
+import { input } from "./utils.js";
+
+const findHighScore = function (scores) {
     function compareNumbers(a, b) {
         return a - b;
     }
@@ -6,10 +8,33 @@ function findHighScore(scores) {
     scores.sort(compareNumbers);
     const highScore = scores[scores.length - 1];
     return highScore;
-}
-
-const scores = [1, 200, 40, 5];
-console.log(findHighScore(scores));
+};
 
 // Simpler way: function findHighScore(scores) { return Math.max(...scores) }
 // ... is 'spread syntax' and expands the array into individual elements (or arguments in this case)
+
+const main = async function () {
+    const scores = [];
+
+    while (true) {
+        const inputStr = await input(
+            "Enter a score. To cancel, type 'done':\n"
+        );
+        if (inputStr === "done") {
+            break;
+        } else {
+            const newScore = Number(inputStr);
+            if (isNaN(newScore)) {
+                console.error(
+                    "That is not a valid score - score must be a number"
+                );
+            } else {
+                scores.push(newScore);
+            }
+        }
+    }
+
+    console.log("The high score is", findHighScore(scores));
+};
+
+main();
