cc664b5c328ae12121e355f056ba152016298dc4
Menaka Maruthainar <menaka.maruthainar@softwire.com>
2025-04-30 14:17:07 +0100

    fix countDown function
    countDown function now times console logs correctly for any input value

--- better-lift-off-countdown.js
+++ better-lift-off-countdown.js
@@ -1,4 +1,4 @@
-function countDown(time) {
+function countDown(time, originalTime = time) {
     if (!Number.isInteger(time) || Math.sign(time) === -1) {
         console.error("Time value is not a positive integer");
         return;
@@ -7,12 +7,12 @@ function countDown(time) {
     if (time === 0) {
         setTimeout(function () {
             console.log("Lift-off!");
-        }, (3 - time) * 1000); // Instead of 3 should be able to take in any positive integer time value
+        }, (originalTime - time) * 1000);
     } else {
         setTimeout(function () {
             console.log(time);
-        }, (3 - time) * 1000);
-        countDown(time - 1);
+        }, (originalTime - time) * 1000);
+        countDown(time - 1, originalTime);
     }
 }
 countDown(3);
